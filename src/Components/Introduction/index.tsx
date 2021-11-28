36f00502edc2fc37c5922b1b512265056cdf00dd
alivarastepour <alivaraste@outlook.com>
2021-11-28 08:08:51 -0500

    link styles edit

--- src/Components/Introduction/index.tsx
+++ src/Components/Introduction/index.tsx
@@ -8,7 +8,7 @@ const Introduction = ()=> {
                 <h1 className='display-2 header'>Welcome to my personal blog !</h1>
                 <div className='details'>
                     Hi everyone. my name is Ali Varaste Pour. I am a student at
-                    <a target='_blank' href="https://www.sbu.ac.ir/" rel='noreferrer'> SBU university of Tehran</a>.
+                    <a target='_blank' className='link' href="https://www.sbu.ac.ir/" rel='noreferrer'> SBU university of Tehran</a>.
                     i spend my time mostly on learning new stuff; as i am young and passionate, im trying to find my area of expertise; as they say:
                     <br/>
                     <br/>
