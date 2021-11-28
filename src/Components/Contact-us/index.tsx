ce436e95128b9b8c16cb1529bedc2e73be61acdc
Ali <alivaraste@outlook.com>
2021-11-28 14:14:35 +0330

    more Contact-us component styles

--- src/Components/Contact-us/index.tsx
+++ src/Components/Contact-us/index.tsx
@@ -4,8 +4,34 @@ import './styles.scss'
 
 const ContactUs = () => {
     return <>
-        <Wrapper className='container glass'>
-
+        <Wrapper className='container glass-contact-us'>
+            <div className='container-header'>Lets stay tuned !</div>
+            <div className='container-body-flex'>
+                <div className="container-flex-item">
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="container-flex-item">
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="container-flex-item">
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="container-flex-item">
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="container-flex-item">
+                    <div></div>
+                    <div></div>
+                </div>
+                <div className="container-flex-item">
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
         </Wrapper>
     </>
 }
