827c5010499787f4ffbbd8a5fbaab9b19338deca
Ali <alivaraste@outlook.com>
2021-11-28 09:52:12 +0330

    Introduction details edit

--- src/Components/Introduction/index.tsx
+++ src/Components/Introduction/index.tsx
@@ -7,9 +7,9 @@ const Introduction = ()=> {
             <div className='grid-item content'>
                 <h1 className='display-2 header'>Welcome to my personal blog !</h1>
                 <div className='details'>
-                    Hi everyone. my name is Ali Varaste Pour. I am a student at
-                    <a target='_blank' className='link' href="https://www.sbu.ac.ir/" rel='noreferrer'> SBU university of Tehran</a>.
-                    i spend my time mostly on learning new stuff; as i am young and passionate, im trying to find my area of expertise; as they say:
+                    Hi everyone. My name is Ali Varaste Pour. I am a student at
+                    <a target='_blank' className='link' href="https://www.sbu.ac.ir/" rel='noreferrer'> Shahid beheshti university of Tehran</a>.
+                      I spend my time mostly on learning new stuff; As i am young and passionate, im trying to find my area of expertise; Like they say:
                     <br/>
                     <br/>
                     <blockquote className='blockquote-footer'>i'm practically new around so don't bother</blockquote>
