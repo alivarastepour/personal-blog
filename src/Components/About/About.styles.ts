344433f35dd3e2dd7a90193bc095e2f3902955ad
Ali <alivaraste@outlook.com>
2021-11-28 10:16:31 +0330

    About component details edit

--- src/Components/About/About.styles.ts
+++ src/Components/About/About.styles.ts
@@ -3,5 +3,6 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   height: 800px;
   max-width: 1600px;
-  margin-top: 200px;  
+  margin-top: 200px;
+  font-family: 'Raleway', sans-serif;
 `;
--- src/Components/About/index.tsx
+++ src/Components/About/index.tsx
@@ -5,9 +5,11 @@ import './styles.scss'
 const About = ()=> {
     return <>
         <Wrapper className='container flex-container'>
-            <div className="card"></div>
-            <div className="card image"></div>
-            <div className="card"></div>
+            <div className="flex-item-About">
+                <div className="card1-header">My story</div>
+                <div className="card1-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi, aperiam cupiditate dolorem eligendi hic ipsum, laudantium minima, molestiae nesciunt nostrum odio quae ratione rem reprehenderit sunt tempore. A alias consequuntur, et hic impedit in necessitatibus nobis nostrum optio quas quibusdam sint tempore vitae! Beatae, hic, veniam. Cumque, expedita, vitae?</div>
+            </div>
+            <div className="flex-item-About image"></div>
         </Wrapper>
     </>
 }
