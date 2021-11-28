6b37c7fc9371e845accffdce81e5df109a0185fd
Ali <alivaraste@outlook.com>
2021-11-28 20:55:35 +0330

    social medias are added to contact-us

--- src/Components/Contact-us/index.tsx
+++ src/Components/Contact-us/index.tsx
@@ -1,36 +1,27 @@
 import {Wrapper} from "./Contact-us.styles";
 import React from "react";
 import './styles.scss'
+import {socialMedias} from "./socialMedia-data";
+
 
 const ContactUs = () => {
+
     return <>
         <Wrapper className='container glass-contact-us'>
-            <div className='container-header'>Lets stay tuned !</div>
             <div className='container-body-flex'>
-                <div className="container-flex-item">
-                    <div></div>
-                    <div></div>
-                </div>
-                <div className="container-flex-item">
-                    <div></div>
-                    <div></div>
-                </div>
-                <div className="container-flex-item">
-                    <div></div>
-                    <div></div>
-                </div>
-                <div className="container-flex-item">
-                    <div></div>
-                    <div></div>
-                </div>
-                <div className="container-flex-item">
-                    <div></div>
-                    <div></div>
-                </div>
-                <div className="container-flex-item">
-                    <div></div>
-                    <div></div>
-                </div>
+                <div className='container-header'>Let's catch up !</div>
+                {
+                    socialMedias.map(acc => {
+                        return <div key={acc.id} className="container-flex-item">
+                            <div className='socialMedia-wrapper'>
+                                <a href={acc.link}>
+                                    <div><i className={acc.class_name}></i></div>
+                                    <div className='socialMedia-name'>{acc.name}</div>
+                                    </a>
+                            </div>
+                        </div>
+                    })
+                }
             </div>
         </Wrapper>
     </>
---
+++ src/Components/Contact-us/socialMedia-data.ts
@@ -0,0 +1,15 @@
+type social = {
+    id: number,
+    name: string,
+    class_name: string,
+    link: string
+}
+
+export const socialMedias:social[] = [
+    {id: 1, name: 'Telegram', class_name: 'fa fa-telegram fa-4x', link:'https://t.me/Swishswhishmdfk'},
+    {id: 2, name: 'Instagram', class_name: 'fa fa-instagram fa-4x', link:'https://instagram.com/10av01'},
+    {id: 3, name: 'LinkedIn', class_name: 'fa fa-linkedin-square fa-4x', link:'https://www.linkedin.com/in/ali-varaste-pour'},
+    {id: 4, name: 'stack overflow', class_name: 'fa fa-stack-overflow fa-4x', link:'https://stackoverflow.com/users/16996769/ali-varaste-pour'},
+    {id: 5, name: 'gmail', class_name: 'fa fa-google fa-4x', link:'https://ali.varaste.pour.12122@gmail.com'},
+    {id: 6, name: 'Github', class_name: 'fa fa-github fa-4x', link:'https://github.com/alivarastepour'},
+]
