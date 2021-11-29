7dc1fc612a6b6f725ed4af0e74c4ab21afe79920
alivarastepour <alivaraste@outlook.com>
2021-11-29 14:15:44 -0500

    typo fix

--- src/Components/Contact-us/socialMedia-data.ts
+++ src/Components/Contact-us/socialMedia-data.ts
@@ -12,6 +12,6 @@ export const socialMedias:social[] = [
     {id: 2, name: 'Instagram', class_name: 'fa fa-instagram fa-4x', link:'https://instagram.com/10av01', newTab:true},
     {id: 3, name: 'LinkedIn', class_name: 'fa fa-linkedin-square fa-4x', link:'https://www.linkedin.com/in/ali-varaste-pour', newTab:true},
     {id: 4, name: 'Stack overflow', class_name: 'fa fa-stack-overflow fa-4x', link:'https://stackoverflow.com/users/16996769/ali-varaste-pour', newTab:true},
-    {id: 5, name: 'Gmail', class_name: 'fa fa-google fa-4x', link:'ali.varaste.pour.12122@gmail.com', newTab:false, msg: 'e-mail copied to clipboard'},
+    {id: 5, name: 'Gmail', class_name: 'fa fa-google fa-4x', link:'ali.varaste.pour.12122@gmail.com', newTab:false, msg: 'Gmail copied to clipboard'},
     {id: 6, name: 'Github', class_name: 'fa fa-github fa-4x', link:'https://github.com/alivarastepour', newTab:true},
 ]
