6aaf349ceecfdf0167a152b49d3e223a96ff96aa
alivarastepour <alivaraste@outlook.com>
2021-11-29 14:14:55 -0500

    concern separation

--- src/Components/Contact-us/copy-to-clipboard.ts
+++ src/Components/Contact-us/copy-to-clipboard.ts
@@ -1,3 +1,10 @@
-export const copyToClipboard = (value: string) => {
+const copyToClipboard = (value: string) => {
     navigator.clipboard.writeText(value).catch(error => console.log(error));
+}
+
+
+export const handleCopy = (value: string, setShow: Function) => {
+    copyToClipboard(value);
+    setShow(true);
+    setTimeout(()=> setShow(false), 2000);
 }
