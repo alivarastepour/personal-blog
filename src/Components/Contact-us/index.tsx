7779b17164f65f01e82f75a4f62f7179955a58eb
alivarastepour <alivaraste@outlook.com>
2021-11-29 14:16:18 -0500

    copied to clipboard notification is added

--- src/Components/Contact-us/index.tsx
+++ src/Components/Contact-us/index.tsx
@@ -1,11 +1,14 @@
 import {Wrapper} from "./Contact-us.styles";
-import React from "react";
+import React, {useState} from "react";
 import './styles.scss'
 import {socialMedias} from "./socialMedia-data";
-import {copyToClipboard} from "./copy-to-clipboard";
+import {handleCopy} from "./copy-to-clipboard";
 
 
 const ContactUs = () => {
+
+    const [show, setShow] = useState(false);
+    const [show1, setShow1] = useState(false);
 
     return <>
         <Wrapper className='container glass-contact-us'>
@@ -13,19 +16,29 @@ const ContactUs = () => {
                 <div className='container-header'>Let's be in touch !</div>
                 {
                     socialMedias.map(acc => {
-                        return <div key={acc.id} className="container-flex-item">
+                        return <div
+                                    key={acc.id}
+                                    className="container-flex-item">
                             <div className='socialMedia-wrapper'>
 
                                 {
                                     acc.newTab ?
-                                        <a href={acc.link} rel='noreferrer' target='_blank'>
-                                        <div><i className={acc.class_name}></i></div>
+                                        <a
+                                            href={acc.link}
+                                            rel='noreferrer'
+                                            target='_blank'
+                                        >
+                                        <div><i className={acc.class_name}>
+                                        </i></div>
                                         <div className='socialMedia-name'>{acc.name}</div>
                                         </a>
                                         :
-                                        <button onClick={() => copyToClipboard(acc.link)}>
-                                            <div><i className={acc.class_name}></i></div>
+                                        <button onClick={() => handleCopy(acc.link, acc.name==='Telegram' ? setShow : setShow1)}>
+                                            <div><i className={acc.class_name}>
+                                            </i></div>
                                             <div className='socialMedia-name'>{acc.name}</div>
+                                            <div
+                                                className={acc.name === 'Telegram' && show ? 'msg msg-show' : acc.name === 'Gmail' && show1 ? 'msg msg-show' : 'msg msg-hide' }>{acc.msg}</div>
                                         </button>
                                 }
                             </div>
