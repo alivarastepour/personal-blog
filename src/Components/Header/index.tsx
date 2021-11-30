50bbffd349d6abdc22252979c47a9dfacd0d6378
alivarastepour <alivaraste@outlook.com>
2021-11-29 20:15:38 -0500

    code cleanup

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,13 +1,18 @@
 import {Wrapper} from "./Header.styles";
-
+import './styles.scss';
 const Header = () => {
     return <>
         <Wrapper>
-            <div className='container flex-container glass'>
-                <button className="flex-item button">change theme</button>
-                <button className='flex-item button'>About</button>
+            <div className='container flex-container glass-header'>
+            <div>
                 <button className='flex-item button'>Home</button>
+                </div>
+                <div>
+                <button className='flex-item button'>About</button>
+                </div>
+                <div>
                 <button className='flex-item button'>Contact us</button>
+                </div>
             </div>
         </Wrapper>
     </>
