91d464b8e79d2aebbad836c4055e9b59d29fcc81
Ali <alivaraste@outlook.com>
2021-11-30 15:52:22 +0330

    loading page random content selector is added

--- src/Components/Spinner/index.tsx
+++ src/Components/Spinner/index.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { Wrapper, Content } from "./Spinner.styles";
+import './styles.scss'
+import {quotes} from "./text-data";
+
 const Spinner = () => {
+    const rand = Math.floor(Math.random() * 1000) % 5;
+    console.log(rand)
+    const value = quotes[rand];
     return <>
-        <Wrapper>
-            <Content />
+        <Wrapper className='wrapper'>
+                <Content className='content'/>
+            <div className='welcome-text'>
+                <div className='text'>{value.text}</div>
+                <blockquote className='blockquote'>{value.name}</blockquote>
+            </div>
         </Wrapper>
     </>
 }
