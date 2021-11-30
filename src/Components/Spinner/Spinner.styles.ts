245b47c677c9a85b50692b1ecc0ad098ab8db078
Ali <alivaraste@outlook.com>
2021-11-30 15:30:27 +0330

    spinner styles are improved

--- src/Components/Spinner/Spinner.styles.ts
+++ src/Components/Spinner/Spinner.styles.ts
@@ -1,20 +1,20 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-    display: flex ;
-    justify-content: center ;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
 `;
 
 export const Content = styled.div`
-    width: 80px;
-    height: 80px;
+    width: 100px;
+    height: 100px;
     border-top: 4px #30508c solid;
     border-right: 4px #3e598c solid;
-    // border-left: 4px black solid ;
     border-bottom: none ;
     animation: 3s spin infinite linear;
     border-radius: 50% ;
-
     @keyframes spin {
         0%{transform: rotate(0deg)}
         100%{transform: rotate(360deg)}
