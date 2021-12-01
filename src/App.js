15815a0fcd45d96a126f30d3bdc49c2bf20f9a18
Ali <alivaraste@outlook.com>
2021-12-01 19:33:07 +0330

    code cleanup

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import './sample.scss'
 import Spinner from "./Components/Spinner";
-import {lazy, Suspense, useState} from "react";
+import {lazy, Suspense} from "react";
+import {useSelector} from "react-redux";
+
 const Header = lazy(() => import('./Components/Header'));
 const About = lazy(() => import('./Components/About'));
 const ContactUs = lazy(() => import('./Components/Contact-us'));
 const Footer = lazy(() => import('./Components/Footer'));
 const Introduction = lazy(() => import('./Components/Introduction'));
-
 function App() {
-    const [loading, setLoading] = useState(true);
-  return<>
-          <Suspense fallback={<Spinner/>}>
-              <div className='App'>
-              <Header/>
-              <Introduction/>
-              <About/>
-              <ContactUs/>
-              <Footer/>
-              </div>
-          </Suspense>
-
-      {/*<div className='App'>*/}
-      {/*    <Spinner/>*/}
-      {/*</div>*/}
+    const theme = useSelector((state => state.theme.value));
+    return<>
+            <Suspense fallback={<Spinner/>}>
+                <div className='App'>
+                    <Header/>
+                    <Introduction/>
+                    <About/>
+                    <ContactUs/>
+                    <Footer/>
+                </div>
+            </Suspense>
     </>
   ;
 }
