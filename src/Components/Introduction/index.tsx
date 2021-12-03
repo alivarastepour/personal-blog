36fefb81404758725733d244d246be8a409ad21c
ali varaste pour <alivaraste@outlook.com>
2021-12-03 03:12:59 -0700

    Introduction details update

--- src/Components/Introduction/index.tsx
+++ src/Components/Introduction/index.tsx
@@ -7,8 +7,8 @@ const Introduction = ()=> {
             <div className='grid-item content'>
                 <h1 className='display-2 header'>Welcome to my personal blog !</h1>
                 <div className='details'>
-                    Hi everyone. My name is Ali Varaste Pour. I am a student at
-                    <a target='_blank' className='link' href="https://www.sbu.ac.ir/" rel='noreferrer'> Shahid beheshti university of Tehran</a>.
+                    Hi everyone. My name is Ali Varaste Pour. I am a student at {}
+                    <a target='_blank' className='link' href="https://www.sbu.ac.ir/" rel='noreferrer'>Shahid beheshti university of Tehran</a>.
                       I spend my time mostly on learning new stuff; As i am young and passionate, im trying to find my area of expertise; Like they say:
                     <br/>
                     <br/>
