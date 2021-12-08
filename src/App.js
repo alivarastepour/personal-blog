3baf9254bf9db2c4235b104c07741579a15a2de3
ali varaste pour <alivaraste@outlook.com>
2021-12-08 12:18:28 -0700

    access to userLocation in entire app is granted

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './sample.scss'
 import Spinner from "./Components/Spinner";
-import {lazy, Suspense} from "react";
+import {lazy, Suspense, useState} from "react";
 import {useSelector} from "react-redux";
 
 const Header = lazy(() => import('./Components/Header'));
@@ -9,7 +9,11 @@ const ContactUs = lazy(() => import('./Components/Contact-us'));
 const Footer = lazy(() => import('./Components/Footer'));
 const Introduction = lazy(() => import('./Components/Introduction'));
 function App() {
+    const [loc, setLoc] = useState();
     const theme = useSelector((state => state.theme.value))?.toLowerCase();
+    useSelector((state1 => state1.userLocation.value.then(r => setLoc(r.location.country))))
+    loc && console.log(loc);
+
     return<>
             <Suspense fallback={<Spinner/>}>
                 <div className={`App ${theme ? `App-${theme}` : `App-default`}`}>
