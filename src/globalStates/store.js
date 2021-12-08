f06dd06dac451390b46e26d537e36b9af04bab5b
ali varaste pour <alivaraste@outlook.com>
2021-12-08 12:17:07 -0700

    store is updated for new reducer

--- src/globalStates/store.js
+++ src/globalStates/store.js
@@ -1,5 +1,6 @@
 import {configureStore} from "@reduxjs/toolkit";
 import themeReducer from './slice'
+import locReducer from "./slice1";
 export default configureStore({
-    reducer:{theme:themeReducer}
+    reducer:{theme:themeReducer, userLocation:locReducer}
 })
