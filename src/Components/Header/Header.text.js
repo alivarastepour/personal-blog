cdac4195790d5e1c4fe61bfbb92d1c2ccb6a5c25
ali varaste pour <alivaraste@outlook.com>
2021-12-10 01:26:17 -0700

    english and persian content of Header is written

---
+++ src/Components/Header/Header.text.js
@@ -0,0 +1,34 @@
+export const en = {
+    'Home':'Home',
+    'About':'About',
+    'Contact us':'Contact us',
+    'Resume':'Resume',
+    'Language':{
+        'Title':'Language',
+        'English':'English',
+        'Persian':'Persian'
+    },
+    'Theme':{
+        'Title':'Theme',
+        'Default':'Default',
+        'Dark':'Dark',
+        'Light':'Light'
+    }
+}
+export const fa = {
+    'Home':'صفحه اصلی',
+    'About':'درباره من',
+    'Contact us':'ارتباط',
+    'Resume':'رزومه',
+    'Language':{
+        'Title':'زبان',
+        'English':'انگلیسی',
+        'Persian':'فارسی'
+    },
+    'Theme':{
+        'Title':'چشم هایت',
+        'Default':'عصر پاییز',
+        'Dark':'سیاهی شب',
+        'Light':'روز روشن'
+    }
+}
