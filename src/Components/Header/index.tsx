3c10ede612361804fc6f8818bad21032e9046d9a
ali varaste pour <alivaraste@outlook.com>
2021-12-10 01:28:11 -0700

    dynamic Header language

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,41 +1,45 @@
-import {Wrapper} from "./Header.styles";
-import './styles.scss';
 import {useDispatch} from "react-redux";
+import { useContext } from "react";
+
+import './styles.scss';
+import {Wrapper} from "./Header.styles";
 import {changeTheme} from "../../globalStates/slice";
 import { languageContext } from "../../App";
-import { useContext } from "react";
+import { en, fa } from "./Header.text";
+
 const Header = () => {
     const {language, setLanguage} = useContext(languageContext);
-    
+    const isPersian = language === 'IR';
+    const content = isPersian ? fa : en ;    
     const dispatch = useDispatch();
     return <>
         <Wrapper>
             <div className='container flex-container glass-header'>
                 <div className="holder-r">
-                <button className='flex-item button'><a href="#intro">Home</a></button>
+                <button className='flex-item button'><a href="#intro">{content['Home']}</a></button>
                 </div>
                 <div className="holder-r">
-                <button className='flex-item button'><a href="#about">About</a></button>
+                <button className='flex-item button'><a href="#about">{content['About']}</a></button>
                 </div>
                 <div className="holder-r">
-                <button className='flex-item button'><a href="#contact-us">contact us</a></button>
+                <button className='flex-item button'><a href="#contact-us">{content['Contact us']}</a></button>
                 </div>
                 <div className="holder-r">
-                <button className='flex-item button'><a rel='noreferrer' href='https://drive.google.com/file/d/1-brDEJyYatceHoFlw_SyJku0VR0Ix0w0/view?usp=sharing' target='_blank'>Resume</a></button>
+                <button className='flex-item button'><a rel='noreferrer' href='https://drive.google.com/file/d/1-brDEJyYatceHoFlw_SyJku0VR0Ix0w0/view?usp=sharing' target='_blank'>{content['Resume']}</a></button>
                 </div>
                 <div className="drop-down holder-r">
-                    <button className="dropbtn flex-item button">language</button>
+                    <button className="dropbtn flex-item button">{content['Language']['Title']}</button>
                         <div className="drop-down-content l">
-                            <button onClick={() => setLanguage('persian')} className="drop-down-button">Persian</button>
-                            <button onClick={() => setLanguage('english')} className="drop-down-button">English</button>
+                            <button onClick={() => setLanguage('persian')} className="drop-down-button">{content['Language']['Persian']}</button>
+                            <button onClick={() => setLanguage('english')} className="drop-down-button">{content['Language']['English']}</button>
                         </div>
                 </div>
                 <div className="drop-down holder-r">
-                    <button className="dropbtn flex-item button">theme</button>
+                    <button className="dropbtn flex-item button">{content['Theme']['Title']}</button>
                         <div className="drop-down-content t">
-                            <button onClick={() => dispatch(changeTheme('LIGHT'))} className="drop-down-button light">Light</button>
-                            <button onClick={() => dispatch(changeTheme('DARK'))} className="drop-down-button dark">Dark</button>
-                            <button onClick={() => dispatch(changeTheme('DEFAULT'))} className="drop-down-button default">Default</button>
+                            <button onClick={() => dispatch(changeTheme('LIGHT'))} className="drop-down-button light">{content['Theme']['Light']}</button>
+                            <button onClick={() => dispatch(changeTheme('DARK'))} className="drop-down-button dark">{content['Theme']['Dark']}</button>
+                            <button onClick={() => dispatch(changeTheme('DEFAULT'))} className="drop-down-button default">{content['Theme']['Default']}</button>
                         </div>
                 </div>
             </div>
