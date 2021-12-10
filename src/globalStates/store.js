8ce9bef30668e31cd663daeb608d50720c37648e
ali varaste pour <alivaraste@outlook.com>
2021-12-09 23:43:24 -0700

    change rollback(issue:unserializable object used in redux states)

--- src/globalStates/store.js
+++ src/globalStates/store.js
@@ -1,6 +1,5 @@
 import {configureStore} from "@reduxjs/toolkit";
 import themeReducer from './slice'
-import locReducer from "./slice1";
 export default configureStore({
-    reducer:{theme:themeReducer, userLocation:locReducer}
+    reducer:{theme:themeReducer},
 })
