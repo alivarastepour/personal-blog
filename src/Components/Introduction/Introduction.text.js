54a1d596e66054e6b49b6d45a65949d66b21edf6
ali varaste pour <alivaraste@outlook.com>
2021-12-10 14:49:19 -0700

    english and persian content of Introduction is written

---
+++ src/Components/Introduction/Introduction.text.js
@@ -0,0 +1,8 @@
+export const en = {
+    'Title':'Welcome to my personal blog !',
+    'Paragraph':'Hi everyone. My name is Ali Varaste Pour. I am a student at Shahid Beheshti University. I spend my time mostly on learning new stuff, reading books and helping others as much as i can; As i am young and passionate, im trying to find my area of expertise; but you know, it ain\'t easy.  '
+}
+export const fa = {
+    'Title':'روز و روزگار خوش!',
+    'Paragraph':'سلام,من علی وارسته پور هستم;این روزا دانشجوی رشته مهندسی کامپیوتر در دانشگاه شهید بهشتی تهرانم.روز هام بیشتر به یادگیری مطالب جدید در حوزه تکنولوژی, کتاب خوندن و کمک کردن به بقیه البته تا جایی که بتونم سپری میشه. علاقه زیادم به برنامه نویسی باعث شده تا همیشه دنبال دایره شایستگیم تو این دنیای بی پایان باشم; که خب, کار زمان بریه.'
+}
