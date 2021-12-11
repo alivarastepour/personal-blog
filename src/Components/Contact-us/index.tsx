97226e561f2e39ec364370eacd9f4b16225b31cf
ali varaste pour <alivaraste@outlook.com>
2021-12-11 11:19:44 -0700

    Contact-us language switch logic is dded

--- src/Components/Contact-us/index.tsx
+++ src/Components/Contact-us/index.tsx
@@ -1,21 +1,25 @@
 import {Wrapper} from "./Contact-us.styles";
-import React, {useState} from "react";
+import  {useState, useContext} from "react";
 import './styles.scss'
-import {socialMedias} from "./socialMedia-data";
+import {en, fa} from "./socialMedia-data";
 import {handleCopy} from "./copy-to-clipboard";
+import { languageContext } from "../../App";
+
 
 
 const ContactUs = () => {
 
     const [show, setShow] = useState(false);
     const [show1, setShow1] = useState(false);
-
+    const {language, setLanguage} = useContext(languageContext);
+    const isPersian = language === 'IR';
+    const content = isPersian ? fa : en ;
     return <>
         <Wrapper id="contact-us" className='container glass-contact-us'>
             <div className='container-body-flex'>
-                <div className='container-header'>Let's stay in touch</div>
+                <div className='container-header'>{isPersian ? 'با من در ارتباط باش' : 'Let\'s stay in touch'}</div>
                 {
-                    socialMedias.map(acc => {
+                    content.map(acc => {
                         return <div
                                     key={acc.id}
                                     className="container-flex-item">
@@ -33,12 +37,12 @@ const ContactUs = () => {
                                         <div className='socialMedia-name'>{acc.name}</div>
                                         </a>
                                         :
-                                        <button onClick={() => handleCopy(acc.link, acc.name==='Telegram' ? setShow : setShow1)}>
+                                        <button onClick={() => handleCopy(acc.link, acc.name==='Telegram' || acc.name==='تلگرام' ? setShow : setShow1)}>
                                             <div><i className={acc.class_name}>
                                             </i></div>
                                             <div className='socialMedia-name'>{acc.name}</div>
                                             <div
-                                                className={acc.name === 'Telegram' && show ? 'msg msg-show' : acc.name === 'Gmail' && show1 ? 'msg msg-show' : 'msg msg-hide' }>{acc.msg}</div>
+                                                className={acc.name === 'Telegram' || acc.name === 'تلگرام' && show ? 'msg msg-show' : acc.name === 'Gmail' || acc.name === 'جیمیل' && show1 ? 'msg msg-show' : 'msg msg-hide' }>{acc.msg}</div>
                                         </button>
                                 }
                             </div>
