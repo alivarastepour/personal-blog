59ab7181d1154383a4f017890b21c541889fdf30
ali varaste pour <alivaraste@outlook.com>
2021-12-11 11:48:38 -0700

    Footer language switch is added

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import {useContext} from "react";
 import {Wrapper} from "./Footer.styles";
 import './styles.scss'
+import { languageContext } from "../../App";
+
 
 const Footer = () => {
+    const {language, setLanguage} = useContext(languageContext);
+    const isPersian = language === 'IR';
     return <>
         <Wrapper className='glass-footer'>
-            <div className='info'>
-                <div>powered by </div>
+            <div className={`info ${isPersian ? 'dirR' : 'dirL'}`}>
+                <div>{isPersian ? 'توسعه با ' : 'powered by '}</div>
                 <i className="fab fa-react animate"></i>
             </div>
         </Wrapper>
