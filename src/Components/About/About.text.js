a54a5e04d062e6a325a6e7ddf3fe8c26eacf94d0
ali varaste pour <alivaraste@outlook.com>
2021-12-11 03:36:16 -0700

    dynamic About language

---
+++ src/Components/About/About.text.js
@@ -0,0 +1,8 @@
+export const en = {
+    'Title':'My story',
+    'Content':'As far as i can remember, i was always interested in the world of technology; but my journey through this world started when a dear frined of mine convinced me to start learning some basics of web development; as my very first exprience it was really wonderful. Then, i had to enter university, which really disappointed me in lots of aspects (but being honest, i learned a lot from students there). So recently i\'ve decided to learn new things all by myself and i\'m super happy with it.'
+}
+export const fa = {
+    'Title':'داستان من',
+    'Content': 'تا اونجایی که حافظم یاری میکنه, یادمه همیشه به هر چیزی که به تکنولوژی یا خودمونی تر بخوام بگم کامپیوتر ربط داشت علاقمند بودم. اما داستان من ازونجایی شروع شد که یکی از دوستام منو متقاعد کرد که توسعه وب رو دنبال کنم; که به عنوان تجربه اول خیلی برام جالب بود. گذشت و گذشت تا وارد دانشگاه شدم که بعد از گدشتن مدتی فهمیدم تنها ویژگی خوبش آدمای جدیدی‌ان که باهاشون آشنا میشی. برای همین تصمیم گرفتم تا هر چیزی که خواستم رو خودم یاد بگیرم; و الان به خاطر این تصمیم خیلی خوشحالم.'
+}
