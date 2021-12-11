ffd7a502e3555592af49b41bb20cbf7041d50258
ali varaste pour <alivaraste@outlook.com>
2021-12-11 03:37:20 -0700

    About direcion is edited

--- src/Components/About/index.tsx
+++ src/Components/About/index.tsx
@@ -1,23 +1,18 @@
+import { useContext } from "react";
 import {Wrapper} from "./About.styles";
 import './styles.scss'
+import { languageContext } from "../../App";
+import { en, fa } from "./About.text";
 
 const About = ()=> {
+    const {language, setLanguage} = useContext(languageContext);
+    const isPersian = language === 'IR';
+    const content = isPersian ? fa : en ;
     return <>
-        <Wrapper id="about" className='container flex-container-about'>
-            <div className="flex-item-About">
-                <div className="card1-header">My story</div>
-                <div className="card1-body">
-                    Many years ago s.one convinced me
-                    to learn front-end development basics
-                    and honestly it was a good experience
-                    as my very first step to the world of technology.
-                    then i entered university and started studying
-                    Computer Engineering. there, i learned 2 new languages;
-                    c++ and java; i really didnt like the former, but the latter
-                    was really interesting. after that i decided to learn new technologies
-                    by myself; since then i am learning reactjs and other related front-end
-                    technologies and i'm SUPER happy with them.
-                </div>
+        <Wrapper id="about" className={`container flex-container-about`}>
+            <div className={`flex-item-About ${isPersian ? 'dirR' : 'dirL'}`}>
+                <div className="card1-header">{content['Title']}</div>
+                <div className="card1-body">{content['Content']}</div>
             </div>
             <div className="flex-item-About image"></div>
         </Wrapper>
