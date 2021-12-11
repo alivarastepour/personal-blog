1892bbfaee105b159636996c79b4fa86aa97ba15
ali varaste pour <alivaraste@outlook.com>
2021-12-11 11:19:07 -0700

    Contact-us support for 2 languages is added

--- src/Components/Contact-us/socialMedia-data.ts
+++ src/Components/Contact-us/socialMedia-data.ts
@@ -7,11 +7,94 @@ type social = {
     msg?: string
 }
 
-export const socialMedias:social[] = [
-    {id: 1, name: 'Telegram', class_name: 'fa fa-telegram fa-4x', link:'@Swishswhishmdfk', newTab:false, msg: 'username copied to clipboard'},
-    {id: 2, name: 'Instagram', class_name: 'fa fa-instagram fa-4x', link:'https://instagram.com/10av01', newTab:true},
-    {id: 3, name: 'LinkedIn', class_name: 'fa fa-linkedin-square fa-4x', link:'https://www.linkedin.com/in/ali-varaste-pour', newTab:true},
-    {id: 4, name: 'Stack overflow', class_name: 'fa fa-stack-overflow fa-4x', link:'https://stackoverflow.com/users/16996769/ali-varaste-pour', newTab:true},
-    {id: 5, name: 'Gmail', class_name: 'fa fa-google fa-4x', link:'ali.varaste.pour.12122@gmail.com', newTab:false, msg: 'Gmail copied to clipboard'},
-    {id: 6, name: 'Github', class_name: 'fa fa-github fa-4x', link:'https://github.com/alivarastepour', newTab:true},
+export const en:social[] = [
+    {
+        id: 1,
+        name: 'Telegram',
+        class_name: 'fa fa-telegram fa-4x', 
+        link:'@Swishswhishmdfk', newTab:false, 
+        msg: 'username copied to clipboard'
+    },
+    {
+        id: 2, 
+        name: 'Instagram', 
+        class_name: 'fa fa-instagram fa-4x', 
+        link:'https://instagram.com/10av01', 
+        newTab:true
+    },
+    {
+        id: 3, 
+        name: 'LinkedIn', 
+        class_name: 'fa fa-linkedin-square fa-4x', 
+        link:'https://www.linkedin.com/in/ali-varaste-pour', 
+        newTab:true
+    },
+    {
+        id: 4, 
+        name: 'Stack overflow', 
+        class_name: 'fa fa-stack-overflow fa-4x', 
+        link:'https://stackoverflow.com/users/16996769/ali-varaste-pour', 
+        newTab:true
+    },
+    {
+        id: 5, 
+        name: 'Gmail', 
+        class_name: 'fa fa-google fa-4x', 
+        link:'ali.varaste.pour.12122@gmail.com', 
+        newTab:false, 
+        msg: 'Gmail copied to clipboard'
+    },
+    {
+        id: 6, 
+        name: 'Github', 
+        class_name: 'fa fa-github fa-4x', 
+        link:'https://github.com/alivarastepour', 
+        newTab:true
+    },
+]
+
+export const fa:social[] = [
+    {
+        id: 1,
+        name: 'تلگرام',
+        class_name: 'fa fa-telegram fa-4x', 
+        link:'@Swishswhishmdfk', newTab:false, 
+        msg: 'نام کاربری به کلیپ‌بورد اضافه شد'
+    },
+    {
+        id: 2, 
+        name: 'اینستاگرام', 
+        class_name: 'fa fa-instagram fa-4x', 
+        link:'https://instagram.com/10av01', 
+        newTab:true
+    },
+    {
+        id: 3, 
+        name: 'لینکدین', 
+        class_name: 'fa fa-linkedin-square fa-4x', 
+        link:'https://www.linkedin.com/in/ali-varaste-pour', 
+        newTab:true
+    },
+    {
+        id: 4, 
+        name: 'استک‌اورفلو', 
+        class_name: 'fa fa-stack-overflow fa-4x', 
+        link:'https://stackoverflow.com/users/16996769/ali-varaste-pour', 
+        newTab:true
+    },
+    {
+        id: 5, 
+        name: 'جیمیل', 
+        class_name: 'fa fa-google fa-4x', 
+        link:'ali.varaste.pour.12122@gmail.com', 
+        newTab:false, 
+        msg: 'آدرس جیمیل به کلیپ‌بورد اضافه شد'
+    },
+    {
+        id: 6, 
+        name: 'گیت‌هاب', 
+        class_name: 'fa fa-github fa-4x', 
+        link:'https://github.com/alivarastepour', 
+        newTab:true
+    },
 ]
