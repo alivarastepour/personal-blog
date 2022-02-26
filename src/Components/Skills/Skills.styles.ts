d45488c89b8368590b80b5e4d17193c7c7f00f57
ali varaste pour <alivaraste@outlook.com>
2022-02-26 03:46:30 -0700

    Skills styles are initialized

---
+++ src/Components/Skills/Skills.styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 90%;
+  margin: 120px auto auto auto;
+  height: 700px;
+  padding: 50px;
+  border-radius: 12px;
+  .header-skills {
+    font-family: "Raleway", "Tanha", sans-serif;
+    text-align: center;
+  }
+`;
