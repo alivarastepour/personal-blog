c1521c92221d58e64db2893346ba1ffe2c337ec6
ali varaste pour <alivaraste@outlook.com>
2022-02-26 04:47:42 -0700

    Fade effect added for Skills on scroll

--- src/Components/Skills/index.tsx
+++ src/Components/Skills/index.tsx
@@ -1,10 +1,14 @@
 import { Wrapper } from "./Skills.styles";
+
 import "./styles.scss";
 
 import { styled } from "@mui/material/styles";
 import Rating from "@mui/material/Rating";
 import CircleOutlinedIcon from "@mui/icons-material/CircleOutlined";
 import CircleRoundedIcon from "@mui/icons-material/CircleRounded";
+
+import { Fade } from "react-awesome-reveal";
+
 import { SKILLS } from "./Skills.text";
 
 const Skills = () => {
@@ -18,28 +22,30 @@ const Skills = () => {
   });
   return (
     <>
-      <Wrapper className="container-skills">
-        <div className="header-skills display-4">skills</div>
-        <div className="flex-skills">
-          {SKILLS.map((skill) => {
-            return (
-              <div className="skill-items">
-                <div className="skill-header">{skill.title}</div>
-                <div>
-                  <StyledRating
-                    key={skill.id}
-                    precision={0.5}
-                    icon={<CircleRoundedIcon color="info" />}
-                    emptyIcon={<CircleOutlinedIcon color="disabled" />}
-                    disabled
-                    value={skill.value}
-                  />
+      <Fade direction="up" delay={50} triggerOnce>
+        <Wrapper className="container-skills">
+          <div className="header-skills display-4">skills</div>
+          <div className="flex-skills">
+            {SKILLS.map((skill) => {
+              return (
+                <div className="skill-items">
+                  <div className="skill-header">{skill.title}</div>
+                  <div>
+                    <StyledRating
+                      key={skill.id}
+                      precision={0.5}
+                      icon={<CircleRoundedIcon color="info" />}
+                      emptyIcon={<CircleOutlinedIcon color="disabled" />}
+                      disabled
+                      value={skill.value}
+                    />
+                  </div>
                 </div>
-              </div>
-            );
-          })}
-        </div>
-      </Wrapper>
+              );
+            })}
+          </div>
+        </Wrapper>
+      </Fade>
     </>
   );
 };
