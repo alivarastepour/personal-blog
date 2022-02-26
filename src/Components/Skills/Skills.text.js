91ec781c3e4d500fa0e29f23a4a57af185fddfd4
ali varaste pour <alivaraste@outlook.com>
2022-02-26 04:43:25 -0700

    constant values are added

---
+++ src/Components/Skills/Skills.text.js
@@ -0,0 +1,15 @@
+export const SKILLS = [
+  { id: 0, title: "Reactjs", value: 2.5 },
+  { id: 1, title: "JavaScript", value: 4 },
+  { id: 2, title: "TypeScript", value: 3 },
+  { id: 3, title: "Nextjs", value: 2 },
+  { id: 4, title: "Redux", value: 2 },
+  { id: 5, title: "Material UI", value: 3 },
+  { id: 6, title: "HTML", value: 5 },
+  { id: 7, title: "CSS", value: 5 },
+  { id: 8, title: "Java", value: 3 },
+  { id: 9, title: "C/C++", value: 3 },
+  { id: 10, title: "Python", value: 3.5 },
+  { id: 11, title: "Git", value: 4 },
+  { id: 12, title: "Linux", value: 4 },
+];
