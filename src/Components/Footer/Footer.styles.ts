3207159199b1d3b2d5baf2ec98842b4655a00e72
Ali <alivaraste@outlook.com>
2023-01-24 23:03:46 +0330

    Footer component is updated

--- src/Components/Footer/Footer.styles.ts
+++ src/Components/Footer/Footer.styles.ts
@@ -2,6 +2,6 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
   max-width: 700px;
-  height: 40px;
+  height: 30px;
   margin: auto;
 `;
--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -1,19 +1,22 @@
-import {useContext} from "react";
-import {Wrapper} from "./Footer.styles";
-import './styles.scss'
+import { useContext } from "react";
+import { Wrapper } from "./Footer.styles";
+import "./styles.scss";
 import { languageContext } from "../../App";
-
-
+import logo from "../../assets/logo.png";
 const Footer = () => {
-    const {language, setLanguage} = useContext(languageContext);
-    const isPersian = language === 'IR';
-    return <>
-        <Wrapper className='glass-footer'>
-            <div className={`info ${isPersian ? 'dirR' : 'dirL'}`}>
-                <div>{isPersian ? 'توسعه با ' : 'powered by '}</div>
-                <i className="fab fa-react animate"></i>
-            </div>
-        </Wrapper>
+  const { language, setLanguage } = useContext(languageContext);
+  const isPersian = language === "IR";
+  const content = "";
+  return (
+    <>
+      <Wrapper className="glass-footer">
+        <div className={`info ${isPersian ? "dirR" : "dirL"}`}>
+          <div>{content}</div>
+          {/* <i className="fab fa-react animate"></i> */}
+          <img src={logo} alt="a" className="footer-logo" />
+        </div>
+      </Wrapper>
     </>
-}
+  );
+};
 export default Footer;
