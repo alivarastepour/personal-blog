8f47a18fe95f6373d0da07d29f3c9d9020cb15d5
Ali <alivaraste@outlook.com>
2023-01-24 23:01:01 +0330

    About component is updated

--- src/Components/About/About.text.js
+++ src/Components/About/About.text.js
@@ -1,10 +1,10 @@
 export const en = {
-  Title: "My story",
+  Title: "Who is HE?",
   Content:
-    "As far as i can remember, i was always interested in the world of technology; but my journey through this world started when a dear frined of mine convinced me to start learning some basics of web development; as my very first exprience it was really wonderful. Then, i had to enter university, which really disappointed me in lots of aspects (but being honest, i learned a lot from students there). So recently i've decided to learn new things all by myself and i'm super happy with it.",
+    "HE was born on a cold winter day in Isfahan in 1380. At the beginning of his life, like all other children, HE tried to learn to walk and talk from his parents. Then HE went to school; Learned addition and multiplication, HE fell down, got up. Lost hope, found it. Then HE lost himself, with more delay this time, HE found it too. HE looked for his interests, went to various classes and after 12 years left school and entered the university environment. These days, HE is busy reading books (HE is very interested in classic novels), watching series, improving social relations and most importantly, programming. and HE hopes to gain work experience in a real project by showing his skills any time soon.",
 };
 export const fa = {
-  Title: "داستان من",
+  Title: "وی کیست؟",
   Content:
-    "تا اونجایی که حافظم یاری میکنه, یادمه همیشه به هر چیزی که به تکنولوژی یا خودمونی تر بخوام بگم کامپیوتر ربط داشت علاقمند بودم. اما داستان من ازونجایی شروع شد که یکی از دوستام منو متقاعد کرد که توسعه وب رو دنبال کنم; که به عنوان تجربه اول خیلی برام جالب بود. گذشت و گذشت تا وارد دانشگاه شدم که بعد از گذشتن مدتی فهمیدم تنها ویژگی خوبش آدمای جدیدی‌ان که باهاشون آشنا میشی و توش چیزی یاد نمیدن. برای همین تصمیم گرفتم تا هر چیزی که خواستم رو خودم یاد بگیرم; و الان به خاطر این تصمیم خیلی خوشحالم.",
+    "وی در یک روز سرد زمستانی در سال 1380 در اصفهان چشم به جهان گشود. در ابتدای زندگی خود مانند تمام بچه‌های دیگر تلاش می‌کرد راه رفتن و سخن گفتن را از محضر اولیای خویش فرا گیرد. سپس به مدرسه رفت؛ جمع و ضرب یاد گرفت، زمین خورد، بلند شد، امید را گم کرد، آن را پیدا کرد. سپس خود را گم کرد، با تاخیری بیشتر این بار، آن را هم پیدا کرد. به دنبال علایق خود گشت، کلاس‌های گوناگون رفت و پس از 12 سال از مدرسه خارج و وارد محیط دانشگاه شد. این روزها او مشغول کتاب خواندن(وی علاقه زیاد به رمان‌های کلاسیک دارد)، سریال دیدن، تقویت روابط اجتماعی و از همه مهم‌تر برنامه‌نویسی می‌باشد و بسیار امیدوار است با نشان دادن شایستگی‌های خود تجربه کار در یک پروژه تجاری با ابعاد واقعی را بدست آورد.",
 };
--- src/Components/About/index.tsx
+++ src/Components/About/index.tsx
@@ -4,6 +4,7 @@ import "./styles.scss";
 import { languageContext } from "../../App";
 import { en, fa } from "./About.text";
 import { Fade } from "react-awesome-reveal";
+import image from "../../assets/photo_2021-11-21_23-43-14.jpg";
 
 const About = () => {
   const { language, setLanguage } = useContext(languageContext);
@@ -11,15 +12,17 @@ const About = () => {
   const content = isPersian ? fa : en;
   return (
     <>
-      <Fade direction="up" delay={50} triggerOnce>
-        <Wrapper id="about" className={`container flex-container-about`}>
-          <div className={`flex-item-About ${isPersian ? "dirR" : "dirL"}`}>
-            <div className="card1-header">{content["Title"]}</div>
-            <div className="card1-body">{content["Content"]}</div>
-          </div>
-          <div className="flex-item-About image"></div>
-        </Wrapper>
-      </Fade>
+      {/* <Fade direction="up" delay={50} triggerOnce> */}
+      <Wrapper id="about" className={`container flex-container-about`}>
+        <div className={`flex-item-About ${isPersian ? "dirR" : "dirL"}`}>
+          <div className="card1-header">{content["Title"]}</div>
+          <div className="card1-body">{content["Content"]}</div>
+        </div>
+        <div className="flex-item-About image-container">
+          <img className="image" src={image} alt="his pic" />
+        </div>
+      </Wrapper>
+      {/* </Fade> */}
     </>
   );
 };
