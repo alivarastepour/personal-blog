d41e8fc417b4b8ae8543c8a6242969dade50cd0a
Ali <alivaraste@outlook.com>
2023-01-24 23:02:09 +0330

    skills component is updated

--- src/Components/Skills/Skills.styles.ts
+++ src/Components/Skills/Skills.styles.ts
@@ -1,69 +1,41 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  width: 80%;
+  width: 100%;
+  padding: 10px;
   margin: 120px auto auto auto;
-  height: 700px;
-  padding: 50px;
   border-radius: 12px;
-  @media only screen and (max-width: 710px) {
-    & {
-      padding: 20px;
-    }
-  }
-  @media only screen and (max-width: 630px) {
-    & {
-      height: 1000px;
-    }
-  }
-  @media only screen and (max-width: 630px) {
-    & {
-      padding: 3px;
-    }
-  }
-  @media only screen and (max-width: 630px) {
-    & {
-      height: 100%;
-    }
-  }
+
   .header-skills {
     font-family: "Raleway", "Tanha", sans-serif;
     text-align: center;
+  }
+  .header-skills.sub-header {
+    font-size: 1.5rem;
   }
   .flex-skills {
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
-    justify-content: center;
-    margin-top: 40px;
-    @media only screen and (max-width: 1330px) {
-      & {
-        margin-top: 20px;
-      }
-    }
+    justify-content: space-around;
+    margin-top: 20px;
   }
-  .skill-items {
-    margin: 50px;
-    display: flex;
-    flex-direction: column;
-    flex-wrap: wrap;
-    transition: transform 0.3s;
-    &:hover {
-      transform: scale(1.05);
-    }
-    @media only screen and (max-width: 1520px) {
-      & {
-        margin: 35px;
-      }
-    }
-    @media only screen and (max-width: 1095px) {
-      & {
-        margin: 15px;
-      }
-    }
-  }
+
   .skill-header {
     margin-bottom: 10px;
     font-size: 1.4rem;
+  }
+
+  .list-item {
+    font-family: "Raleway", "Tanha", sans-serif;
+    font-size: 1.2rem;
+  }
+  .rtl {
+    direction: rtl;
+    text-align: right;
+  }
+  .ltr {
+    display: ltr;
+    text-align: left;
   }
 `;
--- src/Components/Skills/index.tsx
+++ src/Components/Skills/index.tsx
@@ -6,57 +6,81 @@ import "./styles.scss";
 
 import { styled } from "@mui/material/styles";
 import Rating from "@mui/material/Rating";
-import CircleOutlinedIcon from "@mui/icons-material/CircleOutlined";
-import CircleRoundedIcon from "@mui/icons-material/CircleRounded";
 
 import { languageContext } from "../../App";
 
-import { Fade } from "react-awesome-reveal";
-
-import { header, SKILLS } from "./Skills.text";
+import {
+  header,
+  TECHNICAL_SKILLS_EN,
+  TECHNICAL_SKILLS_FA,
+  SOFT_SKILLS_EN,
+  SOFT_SKIILS_FA,
+} from "./Skills.text";
 
 const Skills = () => {
   const { language } = useContext(languageContext);
   const isPersian = language === "IR";
-
-  const StyledRating = styled(Rating)({
-    "& .MuiRating-iconFilled": {
-      color: "black",
-    },
-    "& .MuiRating-iconHover": {
-      color: "#ff3d47",
-    },
-  });
+  const softContent = isPersian ? SOFT_SKIILS_FA : SOFT_SKILLS_EN;
+  const techContent = isPersian ? TECHNICAL_SKILLS_FA : TECHNICAL_SKILLS_EN;
   return (
     <>
-      <Fade direction="up" delay={50} triggerOnce>
-        <Wrapper className="container-skills">
-          <div
-            className={`header-skills ${isPersian ? "display-6" : "display-4"}`}
-          >
-            {isPersian ? header["fa"] : header["en"]}
-          </div>
-          <div className="flex-skills">
-            {SKILLS.map((skill) => {
-              return (
-                <div className="skill-items">
-                  <div className="skill-header">{skill.title}</div>
-                  <div>
-                    <StyledRating
+      {/* <Fade direction="up" delay={50} triggerOnce> */}
+      <Wrapper className="container-skills">
+        <div
+          className={`header-skills ${isPersian ? "display-6" : "display-4"}`}
+        >
+          {isPersian ? header["fa"]["main"] : header["en"]["main"]}
+        </div>
+        <div className="flex-skills">
+          <div>
+            <div
+              className={`header-skills sub-header ${
+                isPersian ? "rtl" : "ltr"
+              }`}
+            >
+              <h2>{isPersian ? header["fa"]["soft"] : header["en"]["soft"]}</h2>
+            </div>
+            <div>
+              <ul>
+                {softContent.map((skill) => {
+                  return (
+                    <li
+                      className={`list-item ${isPersian ? "rtl" : "ltr"}`}
                       key={skill.id}
-                      precision={0.5}
-                      icon={<CircleRoundedIcon color="info" />}
-                      emptyIcon={<CircleOutlinedIcon color="disabled" />}
-                      disabled
-                      value={skill.value}
-                    />
-                  </div>
-                </div>
-              );
-            })}
+                    >
+                      {skill.title}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
           </div>
-        </Wrapper>
-      </Fade>
+          <div>
+            <div
+              className={`header-skills sub-header ${
+                isPersian ? "rtl" : "ltr"
+              }`}
+            >
+              <h2>{isPersian ? header["fa"]["tech"] : header["en"]["tech"]}</h2>
+            </div>
+            <div>
+              <ul>
+                {techContent.map((skill) => {
+                  return (
+                    <li
+                      className={`list-item ${isPersian ? "rtl" : "ltr"}`}
+                      key={skill.id}
+                    >
+                      {skill.title}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </Wrapper>
+      {/* </Fade> */}
     </>
   );
 };
