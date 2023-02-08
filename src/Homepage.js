5aaa522369245d419b9cdc525379da9f573c7068
Ali <alivaraste@outlook.com>
2023-02-08 11:31:02 +0330

    Homepage component is created

---
+++ src/Homepage.js
@@ -0,0 +1,18 @@
+import About from "./Components/About";
+import ContactUs from "./Components/Contact-us";
+import Header from "./Components/Header";
+import Introduction from "./Components/Introduction";
+import Skills from "./Components/Skills";
+
+const Homepage = () => {
+  return (
+    <>
+      <Header />
+      <Introduction />
+      <About />
+      <Skills />
+      <ContactUs />
+    </>
+  );
+};
+export default Homepage;
