3fb4ff7fcd94aeb5f47b4a24348d305a1ab95cce
Ali <alivaraste@outlook.com>
2023-02-08 11:30:35 +0330

    AppWrapper is updated using router

--- src/AppWrapper.js
+++ src/AppWrapper.js
@@ -1,27 +1,41 @@
-import App from "./App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./Homepage";
+import Spinner from "./Components/Spinner";
+import Header from "./Components/Header";
+import { useSelector } from "react-redux";
+import { useState, Suspense, createContext } from "react";
+export const languageContext = createContext();
 
 const AppWrapper = () => {
+  const [language, setLanguage] = useState("IR");
+  const theme = useSelector((state) => state.theme.value)?.toLowerCase();
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <App />,
-      //   children: [
-      //     {
-      //       path: "blog",
-      //       element: <div>salam</div>,
-      //     },
-      //     {
-      //       path: "blog/:blogID",
-      //       element: <div>salam1</div>,
-      //     },
-      //   ],
+      element: <Homepage />,
     },
     {
       path: "blog",
-      element: <div>sa;am</div>,
+      element: <div>salam</div>,
+    },
+    {
+      path: "blog/:blogID",
+      element: <div>salam1</div>,
     },
   ]);
-  return <RouterProvider router={router} />;
+
+  return (
+    <>
+      <Suspense fallback={<Spinner />}>
+        <languageContext.Provider value={{ language, setLanguage }}>
+          <div className={`App ${theme ? `App-${theme}` : `App-default`}`}>
+            {/* <Header /> */}
+            <RouterProvider router={router} />
+            {/* <Outlet /> */}
+          </div>
+        </languageContext.Provider>
+      </Suspense>
+    </>
+  );
 };
 export default AppWrapper;
